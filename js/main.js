1ec9febfa71a8979741a52ce661fb2c1a9862b21
Murad500 <murad.hemzeyev5005@gmail.com>
2023-04-20 13:01:42 +0400

    first

---
+++ js/main.js
@@ -0,0 +1,961 @@
+'use strict';
+
+// let a=0
+// let aye=['cup', 'plate', 'spoon', 'knife', 'fork']
+// while (a<aye.length) {
+//     console.log(aye[a]);
+//     a++
+// }
+
+// let a= 0
+// let aye=['xiyar', 'pomidor', 'sogan', 'sarimsaq', 'semeni']
+// while (a < aye.length) {
+//  if (aye[a][0] === "s"){
+//     console.log(aye[a])
+//  }
+//  a++
+// }
+
+// function balaca herfle yazilir herfle baslamalidir
+
+// function test( age = 20, student) {
+//    return age + " " + student
+// }
+// console.log(test( undefined,"Murad"));
+
+// let array =['cup', 'plate', 'spoon', 'knife', 'fork']
+// for (let index = 0; index < array.length; index++) {
+//    console.log(array[index]);
+// }
+
+// let array = ['xiyar', 'pomidor', 'sogan' ,'sarimsaq', 'semeni']
+// for (let index = 0; index < array.length; index++) {
+//    if(array[index][0]=== 's'){
+//       console.log(array[index]);
+//    }
+// }
+// let total = 0
+// for (let index = 0; index < 101; index++) {
+
+//    if (index % 2 == 0) {
+//       total += index
+//    }
+// }
+// console.log(total);
+// let total = 0
+// for (let i = 0; i < 101; i++) {
+// total += i   
+// }
+// console.log(total);
+
+// function pover(a,b) {
+//    return a**b
+// }
+// console.log(pover(2,4));
+
+// function factorial(a) {
+
+//    if (a == 0) {
+//       return 1
+//    }
+
+//    else{
+//       return (a * factorial(a-1))
+//    }
+
+// }
+// console.log(factorial(5));
+
+
+
+// let a = 0
+// let arrey = ['cup', 'plate', 'spoon', 'knife', 'fork']
+
+// while (a < arrey.length) {
+//     console.log(arrey[a]);
+//     a++
+// }
+
+// let a = 0
+// let arrey = ['xiyar', 'pomidor', 'sogan', 'sarimsaq', 'semeni']
+// while (a < arrey.length) {
+//     if (arrey[a][0] === 's') {
+//         console.log(arrey[a]);
+//     }
+//     a ++
+// }
+
+// let a = 0
+// let array = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq']
+// while (a < array.length) {
+//     let b = array[a];
+//     a++;
+//     if (b.length !== 5) {
+//         continue;
+//     }
+//     let netice = b[b.length - 1] 
+
+//     console.log(netice);
+// }
+// function mango(a, b) {
+
+
+//     if (a % 2 === 1, b > 0) {
+//         if (a < 3, b > 0 ) {
+//             return "1 manqo qazandiniz "+ "\n" + ((a-1)*b) + " AZN odenis etmelisiniz";
+//         }
+//        if (a > 3, b > 0) {
+//            return  ((a-1)/2)-1 + " mango qazandiniz "+ "\n" + a*b + " AZN odenis etmelisiniz";
+//         }
+
+//     }
+
+
+// }
+// console.log(mango(9, 5));
+
+// function evenOrOdd(number) {
+//  if (number % 2 == 1) {
+//     return "Even"
+//  } 
+//  else{
+//     return "Odd"
+//  }
+// }
+// console.log(evenOrOdd(7));
+
+// let a = [1, 3, 5, 9]
+// let max = a[0]
+// for (let i = 0; i < a.length; i++) {
+
+//     if (a[i] > max) {
+//         max = a[i]
+//     }
+
+// }
+// console.log(max);
+
+
+// function love(a, b) {
+//     if (a%2===1 && b%2===0) {
+//         return true
+//     }
+//     else if (a%2===1 && b%2===1) {
+//         return false
+//     }
+//     else if (a%2===0 && b%2===0) {
+//         return false
+//     }
+// }
+// console.log(love(0, 0));
+
+
+
+// let vovel = ["a", "e", "i", "o", "u"]
+//   let a = 0;
+//   for (let index = 0; index < str.length; index++) {
+//     for (let j = 0; j < vovel.length; j++) {
+
+//       if (str[index] == vovel[j]) {
+//         a++
+//       }
+//     }
+//   }
+
+//   return a
+
+
+/*console.log(1==false && 1==false);*/
+
+///////////////////////////////////////////////////////////////////////                                
+// function run() {
+//   return "5 saniyeden bir isleyir"
+// }
+
+// setTimeout(() => {
+//   console.log(run());
+// }, 5000);
+
+
+
+// function run() {
+//   setTimeout(() => {
+//     console.log("5 saniyeden bir isleyir");
+//   }, 5000);
+// }
+// run()
+///////////////////////////////////////////////////////////
+
+// let a = 2129
+// let b = 2023
+// let c = a - b
+// let d = c*24
+// let e = d*10
+// console.log(e);
+
+// let suret = 10
+// function year(a,b) {
+//   return (a-b)*24*suret + " km"
+// }
+// console.log(year(2129,2023));
+
+
+
+// tapsiriqlar        for            while   
+
+// 1ci sual
+
+// let a = ['cup','plate','spoon','knife','fork']
+// let b = 0
+// while (b < a.length) {
+//   console.log(a[b]);
+//   b++
+// }
+
+
+// let a = ['cup','plate','spoon','knife','fork']
+// for (let i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
+
+///////////////////////////////////////////////
+
+// 2ci sual
+// let a = ['xiyar','pomidor','sogan','sarimsaq','semeni','yerkoku']      
+// let b = 0
+// while (b < a.length) {
+//   b++
+//   if (a[b][0] === 's') {
+//     console.log(a[b]);
+//   }
+// }      
+
+// let a = ['xiyar','pomidor','sogan','sarimsaq','semeni','yerkoku']
+// for (let i = 0; i < a.length; i++) {
+//   if (a[i][0] === 's') {
+//     console.log(a[i]);
+//   }
+
+// }
+
+///////////////////////////////////////
+
+//  3cu sual
+// let a = 0
+// let b = 0
+// while (b < 101) {
+//   a+=b
+//   b++
+// }
+// console.log(a);
+
+// let a = 0
+// for (let i = 0; i < 101; i++) {
+//   a += i
+// }
+//   console.log(a);
+
+//////////////////////////////////////////////
+
+//  4cu sual
+// let a = ['kitab', 'defter', 'qelem', 'karandas', 'jurnal', 'pozan', 'vereq']
+// let b = 0
+// while (b < a.length) {
+//   let c = a[b];
+//   b++
+//   if (!c.length == 5) {
+//     continue;
+//   }
+//   let netice = c[c.length - 1]
+//   console.log(netice);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Objact.enties()
+
+// let a = {0 : 'kitab', 1: 'defter',2: 'qelem',3: 'karandas',4: 'jurnal',6: 'pozan',5: 'vereq'}
+
+// console.log(Object.entries(a));
+
+
+// const obj4 = { name: "John", age: 27, location: "Nepal" };
+
+// for (const [key, value] of Object.entries(obj4)) {
+//   console.log(`${key}: ${value}`);
+// }
+
+
+// let a = "code";
+// console.log(Object.entries(a));
+
+
+//Object.values()
+
+// let a = ["JavaScript", "Python", "C"];
+// console.log(Object.values(a)); 
+
+
+// let a = { 65: "A", 66: "B", 67: "C" };
+// console.log(Object.values(a));
+
+
+// let a = "code";
+// console.log(Object.values(a));
+
+
+//Object.keys()
+
+// let a = ["JavaScript", "Python", "C"];
+// console.log(Object.keys(a)); 
+
+
+// let a = { 65: "A", 66: "B", 67: "C" };
+// console.log(Object.keys(a));
+
+
+// let a = "code";
+// console.log(Object.keys(a));
+
+
+//Object.assign()
+
+// const a = {
+//   name: "Alan Turing",
+//   age: 120,
+// };
+
+// let newObject = {};
+
+// const copy = Object.assign(newObject, a);
+// console.log(newObject);
+// console.log(copy);
+
+// ?????????????????????????????????????????????????????
+// const o1 = { a: 1, b: 2, c: 3 };
+// const o2 = { b: 12, c: 13 };
+// const o3 = { c: 23 };
+
+
+// const o4 = Object.assign({}, o1, o2, o3);
+
+// console.log(o4);
+// ?????????????????????????????????????????????????
+//??????????????????????????????????????????????????????????????
+// const obj = Object.create(
+//   { a: 1 },
+//   {
+
+//     b: {
+//       value: 2,
+//     },
+//     c: {
+//       value: 3,
+//       enumerable: true,
+//     },
+//   }
+// );
+
+// let copy = Object.assign({}, obj);
+// // console.log(copy); // { c: 3 }
+
+// copy = Object.assign(copy, "abc", 100, null);
+// console.log(copy); // { '0': 'a', '1': 'b', '2': 'c', c: 3 }
+//??????????????????????????????????????????????????????????????????????????
+
+
+                                  //Array.from()
+
+
+
+//Array.from(obj, mapFunc, thisArg)   ilə   bərabərdir   Array.from(obj).map(mapFunc, thisArg).
+
+
+// let a = Array.from("abc");
+// console.log(a);
+
+////////////////////////
+// arraylike = massiv
+
+// let a= Array.from("JavaScript");
+
+// console.log(a); 
+
+/////////////////////////////////////
+
+// mapFunc
+
+// function createArray(arraylike, mapFunc) {
+//   return Array.from(arraylike, mapFunc);
+// }
+
+// let result = createArray([2, 4, 6], (element) => element + 2);
+
+// console.log(result);
+///////////////////////////////////
+
+//thisArg
+
+// let set = new Set(["JavaScript", "Python", "Go", "Python"]);
+
+// let result = Array.from(set);
+
+// console.log(result);
+
+
+                                  // isArray()
+
+
+// let numbers = [1, 2, 3, 4]; 
+
+// console.log(Array.isArray(numbers));  //true
+
+
+// let text = "JavaScript";
+
+// console.log(Array.isArray(text));  //false
+
+
+// console.log(Array.isArray([])); // true 
+
+
+// console.log(Array.isArray(new Array(7))); // true
+
+
+// console.log(Array.isArray(true)); // false
+
+
+// console.log(Array.isArray(undefined)); // false
+
+
+// console.log(Array.isArray()); // false 
+
+
+
+                                      //if else
+
+// let marks = prompt('Enter your marks :');
+
+
+// let result = (marks >= 40) ? 'pass' : 'fail';
+
+// console.log(`You ${result} the exam.`); 
+
+
+///////////////////////////////////////////
+// let age = 15;
+// let result;
+
+// if (age >= 18) {
+//       result = "You are eligible to vote.";
+// } else {
+//       result = "You are not eligible to vote yet.";
+// }
+
+// console.log(result);
+
+///////////////////////////////////////////////////
+// let age = 15;
+// let result =
+//     (age >= 18) ? "You are eligible to vote." : "You are not eligible to vote yet";
+// console.log(result);
+/////////////////////////////////////
+
+
+// let a = 3;
+// let result = (a >= 0) ? (a == 0 ? "zero" : "positive") : "negative";
+// console.log(`The number is ${result}.`);
+
+
+                                 //switch case
+
+// let a = 2;
+
+// switch (a) {
+
+//     case 1:
+//         a = 'one';
+//         break;
+//     case 2:
+//         a = 'two';
+//         break;
+//     default:
+//         a = 'not found';
+//         break;
+// }
+// console.log(`The value is ${a}`);
+
+
+                         //1
+// let a = ['kitab','defter','qelem','pozan'];
+
+// a.splice( 2,1, 'jurnal','karandas')
+// console.log( a );
+
+// let a = '3'
+// let b = 3
+
+// console.log(a===b);
+
+// a.every(x=> {})
+// console.log(a);
+
+// function arr(a) {
+//     return a % 2 === 0
+
+// }
+// let b = [2,5,32,12,6]
+
+// let c = b.every(arr)
+// console.log(c);
+
+
+
+// let b = [5,34,3]
+
+
+// let a = [-23,5,34,3,-3,0]
+// function musbet(b) {
+//     if (b>=0) {
+//         return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+// let c = a.filter(musbet)
+// let f = console.log(c);
+
+// let d = a.sort(f)
+// console.log(d);
+
+
+// let a = "Save"
+
+// switch (a) {
+//     case "Save":
+//         a = "yadda saxlanildi";
+//         break;
+
+//         case "Cancel":
+//             a = "Cixildi";
+//         break;
+
+//         case "Uptade":
+//             a = "deyisiklikler yadda saxlanildi";
+//         break;
+//     default:
+//         a = "Qebul edilmedi";
+//         break;
+// }
+// console.log(a);
+
+// let a = ['4','6','3','2','5','7']
+// let c = (a.values %2 === 0) ? "cut" : "tek"
+// console.log(c);
+
+
+
+// let age = 15;
+// let result =
+//     (age >= 18) ? "You are eligible to vote." : "You are not eligible to vote yet";
+// console.log(result);
+
+
+
+// let a = Array.from('abcfdeg')
+// console.log(a);
+
+
+// let a = {cloud:'bulud',book:'kitab',juice:'meyve siresi'}
+// let b = {phone:'telefon',window:'pencere'}
+// let c = a.Array.join(b) 
+// console.log(c);
+
+// let a = ['apple','banana','orange']
+// console.log(a);
+
+// let data = ['apple','banana','orange'];
+// let list = document.getElementById("ulli");
+
+
+
+
+
+
+// data.forEach((item)=>{
+//     let li = document.createElement("li");
+//     li.innerText = item;
+//     list.appendChild(li)
+// }) 
+ 
+
+
+
+
+
+
+
+
+
+
+// const uniTotal = str => [...str].reduce((acc, char) => acc + char.charCodeAt(0), 0);
+
+// function uniTotal(str) {
+//     var count = 0;
+//     for (var i = 0; i < str.length; i++) {
+//       count += str.charCodeAt(i);
+//     }
+//     return count;
+//   }  
+
+// function uniTotal(string) {
+//     return Array.prototype.reduce.call(string, (a, c) => a + c.charCodeAt(0), 0);
+//   }
+
+// function uniTotal(str){
+//     return str.split('').reduce(function(last,now){
+//       return last+now.charCodeAt();
+//     },0);
+//   }
+
+// function uniTotal(a)
+// {
+//   return a.split('').reduce((s,c) => s+c.charCodeAt(),0)
+// }
+
+// var uniTotal = x => [...x].map(e=> e.charCodeAt()).reduce((a,b)=> a + b, 0) || 0
+
+
+
+
+
+
+
+
+
+
+
+
+// let bosluq = [];
+
+// function soz(a) {
+//     let bosluq = [];
+  
+//     const sait = ['a', 'e', 'i', 'o', 'u','ə','ı','ö','ü'];
+  
+//     let bos = '';
+  
+
+//     for (let i = 0; i < a.length; i++) {
+//       const letter = a[i];
+
+//       bos += letter;
+  
+//       if (sait.includes(letter) || i === a.length - 1) {
+//         bosluq.push(bos);
+//         bos = '';
+//       }
+//     }
+  
+//     return bosluq;
+//   }
+//   bosluq.push(soz('Orxan'));
+//   bosluq.push(soz('ailə'));
+//   bosluq.push(soz('prius'));
+//   bosluq.push(soz('eynək'));
+//   bosluq.push(soz('elektrikləşdirdiklərimizdəndir'));
+//   for (let i = 0; i < bosluq.length; i++) {
+//     console.log(`SOZ${i+1}: ${bosluq[i].join('-')}`);
+  // }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const syllableRegex = /[^aeiouyəıüö]*[aeiouyəıüö]+(?:[^aeiouyəıüö]*$|[^aeiouyəıüö](?=[^aeiouyəıüö]))?/gi;
+
+// function syllabify(words) {
+//     return words.match(syllableRegex);
+// }
+
+// console.log(['Orxan', 'ailə', 'prius', 'eynək', 'elektrikləşdirdiklərimizdəndir','alma',].map(syllabify))
+
+
+
+
+
+// let a = 0
+
+// for (let a = 0; a <= 40; a++) {
+    
+//     // i+=a
+//     // a++
+//     if (a==1) {
+//         console.log(a + 'sadedir');
+//     }
+//     if (a==3) {
+//         console.log(a + 'sadedir');
+//     }
+//     if (a==2) {
+//         console.log(a + 'sadedir');
+//     }
+//     if(a>3){
+
+//         if (a%a==0) {
+//             if(a%1==0){
+//                 if(a%2==0 ){
+//                     console.log(a + 'murekkebdir');
+//                 }
+//                 if (a%3==0) {
+//                     console.log(a + 'murekkebdir');
+//                 }
+//                 else{
+//                     console.log(a + 'sadedir');
+
+//                 }
+//             }
+//         }
+//     }
+    
+// }
+
+
+// function isPrime(num) {
+//     if (num <= 1) {
+//       return false;
+//     }
+  
+//     // Check for divisors up to the square root of the number
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i === 0) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   }
+  
+//   // Loop through numbers from 1 to 40 and check if they are prime
+//   for (let i = 1; i <= 40; i++) {
+//     if (isPrime(i)) {
+//       console.log(i + " sadedir");
+//     } else {
+//       console.log(i + " murekkebdir");
+//     }
+//   }
+  
+// let num = 0
+
+// function isPrime(num) {
+//     // if (num <= 1) {
+//     //   return false;
+//     // }
+  
+//     // Check for divisors up to the square root of the number
+//     for (let i = 2; i <= Math.sqrt(num); i++) {
+//       if (num % i === 0) {
+//         return false;
+//       }
+//     }
+  
+//     return true;
+//   }
+  
+//   // Loop through numbers from 1 to 40 and check if they are prime
+//   for (let i = 1; i <= 40; i++) {
+//     if (isPrime(i)) {
+//       console.log(i + " sadedir");
+//     } else {
+//       console.log(i + " murekkebdir");
+//     }
+//   }
+
+
+// let a = ""
+
+// function revers(a) {
+
+//   return a.reverse()
+
+// }
+
+// console.log(revers(543));
+
+
+// const fruits = [543];
+// let a = fruits.reverse();
+// console.log(a);
+
+// let a = 0
+// let b = 439
+// a = Number(String(b).split('').reverse().join(''));
+
+
+
+// console.log(a);
+
+                      // Musbet ededi menfi edir,  // Math.abs() musbet edir //
+
+// function makeNegative(num) {
+//   if(num <= 0){
+//     return num;
+//   }else{
+//     return -Math.abs(num);
+//   }
+// }
+// console.log(makeNegative(-7));
+
+               // Qisasi
+
+// function makeNegative(num) {
+//   return -Math.abs(num);
+// }
+
+
+            // Arrayden kiciyini ekrana cixartmaq  // min max deyisende boyuku cixardir 
+
+// const arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+// const min = Math.min(...arr)
+// console.log(min)
+
+// var arr = [14, 58, 20, 77, 66, 82, 42, 67, 42, 4]
+// var min = Math.min.apply(Math, arr)
+// console.log(min)
+
+ 
+                 // Ardicil ededin cemi
+
+// var summation = function (num) {
+//   let sum = 0
+//   for(let i = 0; i <= num; i++) {
+//       sum += i
+//   }
+//   return sum
+// }
+// console.log(summation(8));
+
+
+// function summation(num) {
+//   return num * (num + 1) / 2
+// }
+
+
+                // stringin 1 deneseini tekrar vermesi
+
+// var a = "abc";
+
+// console.log(
+//   a.split('').map(function(b) {
+//     return b + b;
+//   }).join('')
+// )
+
+// let a = "abc";
+// let b = "";
+
+// for (let i = 0; i < a.length; i++) {
+  
+//   b = b + (a[i] + a[i]) 
+
+// }
+// console.log(b);
+
+// function doubleChar(str) {
+//   var newStr = '';
+//   for (var i = 0; i < str.length; i++){
+//     newStr += str[i] + str[i];
+//   }
+//   return newStr;
+// };
+
+
+// const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+
+
+// let str1 = "Hire the top 1% freelance developers";
+
+// const split_string = str1.split(" ");
+// console.log(split_string)
+
+//Output = ["Hire", "the", "top", "1%", "freelance", "developers"]
+
+// function stringToArray(string){
+//   var newString = string.split(" ")
+// }
+
+
+
+// function quvvet(a,b) {
+//   return a**b
+// }
+// console.log(quvvet(3,4));
+
+            //  Vurma
+
+// let a = 1;
+// let b = 2;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(a*i);
+//   console.log(b*i);
+// } 
+
+
+
+// let alfabet = ['A','C','F','B','W','E','Q','R','T','Y','U','I','O','P','S','D','G','H','J','K','L','Z','X','V','N','M']
+// console.log(alfabet.sort());
+
+// let a = "Murad"
+
+// console.log(a.split('').reverse());
+
+
+
+// let name = "Murad";
+
+
+
+let bal=89; 
+
+if (bal>=51 && bal<61) {
+  console.log("E");
+}else if (bal>=61 && bal<71) {
+  console.log("D");
+}else if (bal>=71 && bal<81) {
+  console.log("C");
+}else if (bal>=81 && bal<91) {
+  console.log("B");
+}else if (bal>=91 && bal<=100) {
+  console.log("A");
+}else{
+  console.log("Kesildin");
+}
+
+
+
+
